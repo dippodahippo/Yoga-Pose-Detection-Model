3a6c741be09bc2c6ba28bc26a23ff1192137af82
Diptorshi Tripathi <145859076+dippodahippo@users.noreply.github.com>
2024-05-07 13:18:49 +0530

    Add files via upload

--- pyduinointegr/pyduinointegr.ino
+++ pyduinointegr/pyduinointegr.ino
@@ -1,24 +1,24 @@
-const int led = 13;
+const int led = LED_BUILTIN;
+char msg;
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-
+  digitalWrite(led, LOW);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String msg = Serial.readString();
-
-    if (msg == "ON"){
-      digitalWrite(led, HIGH);
+    msg = Serial.read();
+    Serial.println(msg);
+    
+    if (msg == '1'){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(500);
     }
 
-    else if (msg == "OFF") {
-      digitalWrite(led, LOW);
-    }
-
-    else {
+    else if (msg == '0') {
       digitalWrite(led, LOW);
       for(int i = 0; i < 5; i++){
         digitalWrite(led, HIGH);
@@ -27,6 +27,13 @@ void loop() {
         delay(100);
         }
     }
+
+    else {
+      digitalWrite(led, LOW);
+      delay(10000);
+    }
+  } else {
+    digitalWrite(led, LOW);
   }
 
 }
